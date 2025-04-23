76444c573efef565cf03f42908a820d5eeb9296e
Teo <75574673+sunsakis@users.noreply.github.com>
2025-04-23 20:43:14 +0300

    update

--- zk-crowdfunding-frontend/public/conf/config.js
+++ zk-crowdfunding-frontend/public/conf/config.js
@@ -5,6 +5,5 @@ window.config = {
     rpcNodeUrl: "https://node1.testnet.partisiablockchain.com",
     browserUrl: "https://browser.testnet.partisiablockchain.com"
   },
-  contractAddress: "032bddd6b19a30bf690066e182c23be380e6ed7bee",  // Set this to your deployed contract address
   testMode: false
 };
