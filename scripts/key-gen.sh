af241231f1e7cbcd6b52a2910a0820a949ffb3e6
Teo <75574673+sunsakis@users.noreply.github.com>
2025-05-05 16:40:58 +0300

    added README

--- scripts/key-gen.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cargo partisia-contract account create --file Account-A.pk
-cargo partisia-contract account create --file Account-B.pk
-cargo partisia-contract account create --file Account-C.pk
