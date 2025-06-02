aec5a64d51b696d6b27a376106155a927e9960ec
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2025-06-02 14:11:24 -0500

    feat(ui): :sparkles: Replace icon in NavBar with HandMetal

--- givtisia/src/components/shared/NavBar.tsx
+++ givtisia/src/components/shared/NavBar.tsx
@@ -1,12 +1,12 @@
 import { NavLink } from "react-router";
 import ConnectButton from "@/components/shared/ConnectButton";
-import { FlameKindling } from "lucide-react";
+import { HandMetal } from "lucide-react";
 
 const NavBar = () => {
   return (
     <nav className="flex justify-between items-center p-4">
       <NavLink to="/" className="flex items-center space-x-1">
-        <FlameKindling className="w-6 h-6" />
+        <HandMetal className="w-6 h-6" />
         <p className="text-2xl font-medium tracking-[-0.075rem]">givtisia</p>
       </NavLink>
       <ConnectButton />
